4677e7202cc2121f615dc11f3b0ff6ac7fa209b8
Bima Aryadinata <65204321+ExRen@users.noreply.github.com>
2023-07-17 13:31:24 +0700

    biji

--- resources/views/Landing/landing.blade.php
+++ resources/views/Landing/landing.blade.php
@@ -4,5 +4,15 @@
     <h1>Hi</h1>
     <h2>valy stress</h2>
 </div>
+<div>
+    <ul>
+        <li>Beranda</li>
+        <li>Produk Hukum</li>
+        <li>Pengaduan</li>
+    </ul>
+</div>
+<div>
+    
+</div>
     
 @endsection
