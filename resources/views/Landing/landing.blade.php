44accb7d16473e6f0ca03077a50524f23f49b040
Bima Aryadinata <65204321+ExRen@users.noreply.github.com>
2023-07-18 09:00:36 +0700

    update list

--- resources/views/Landing/landing.blade.php
+++ resources/views/Landing/landing.blade.php
@@ -13,21 +13,78 @@
 </div>
 <div class="peraturan">
     <h2>Peraturan</h2>
-    <br>
     <ul>
-        <li>Raperda</li>
-        <li>Peraturan Daerah (RAPERDA)</li>
-        <li>Peraturan Kepala Daerah</li>
-        <li>Keputusan Kepala Daerah</li>
-        <li>Peraturan Bersama kepala Daerah</li>
-        <li>Peraturan DPRD Provinsi</li>
-        <li>Surat Edaran Kepala Daerah</li>
-        <li>MoU/Kerjasama Antar Daerah</li>
+        <li>Raperda
+            <ul>
+                <li>2023</li>
+                <li>2022</li>
+                <li>2021</li>
+            </ul>
+        </li>
+        <li>Peraturan Daerah (RAPERDA)
+            <ul>
+                <li>2023</li>
+                <li>2022</li>
+                <li>2021</li>
+            </ul>
+        </li>
+        <li>Peraturan Kepala Daerah
+            <ul>
+                <li>2023</li>
+                <li>2022</li>
+                <li>2021</li>
+            </ul>
+        </li>
+        <li>Keputusan Kepala Daerah
+            <ul>
+                <li>2023</li>
+                <li>2022</li>
+                <li>2021</li>
+            </ul>
+        </li>
+        <li>Peraturan Bersama kepala Daerah
+            <ul>
+                <li>2023</li>
+                <li>2022</li>
+                <li>2021</li>
+            </ul>
+        </li>
+        <li>Peraturan DPRD Provinsi
+            <ul>
+                <li>2023</li>
+                <li>2022</li>
+                <li>2021</li>
+            </ul>
+        </li>
+        <li>Surat Edaran Kepala Daerah
+            <ul>
+                <li>2023</li>
+                <li>2022</li>
+                <li>2021</li>
+            </ul>
+        </li>
+        <li>MoU/Kerjasama Antar Daerah
+            <ul>
+                <li>2023</li>
+                <li>2022</li>
+                <li>2021</li>
+            </ul>
+        </li>
     </ul>
 </div>
+{{-- <div class="dropdown show">
+    <a class="btn btn-secondary dropdown-toggle" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      Dropdown link
+    </a>
+  
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+      <a class="dropdown-item" href="#">Action</a>
+      <a class="dropdown-item" href="#">Another action</a>
+      <a class="dropdown-item" href="#">Something else here</a>
+    </div>
+  </div> --}}
 <div>
     <h3>Peraturan Terbaru</h3>
-    <br>
     <ul>
         <li></li>
     </ul>
@@ -38,10 +95,8 @@
         (0711) 352388
         birohukumsumsel@yahoo.com
     </p>
-    <br>
     <p>Biro Hukum dan HAM Setda Provinsi Sumatera Selatan</p>
 
-    <br>
     <h4>Link Terkait</h4>
     <ul>
         <li><a href ="instagram.com" target="_blank">Republik Indonesia</a></li>
