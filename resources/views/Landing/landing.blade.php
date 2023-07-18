307f6a0895d5efcedb98067cb1b6f8aedfd0995d
Bima Aryadinata <65204321+ExRen@users.noreply.github.com>
2023-07-18 08:48:06 +0700

    landing

--- resources/views/Landing/landing.blade.php
+++ resources/views/Landing/landing.blade.php
@@ -1,18 +1,55 @@
 @extends('Layout/index')
 @section('isi')
-<div>
-    <h1>Hi</h1>
-    <h2>valy stress</h2>
+<div class="">
+    
 </div>
-<div>
+<div class="tab-bar">
     <ul>
         <li>Beranda</li>
         <li>Produk Hukum</li>
         <li>Pengaduan</li>
+        <li><input type="search" id="gsearch" name="gsearch">
+    </ul>
+</div>
+<div class="peraturan">
+    <h2>Peraturan</h2>
+    <br>
+    <ul>
+        <li>Raperda</li>
+        <li>Peraturan Daerah (RAPERDA)</li>
+        <li>Peraturan Kepala Daerah</li>
+        <li>Keputusan Kepala Daerah</li>
+        <li>Peraturan Bersama kepala Daerah</li>
+        <li>Peraturan DPRD Provinsi</li>
+        <li>Surat Edaran Kepala Daerah</li>
+        <li>MoU/Kerjasama Antar Daerah</li>
     </ul>
 </div>
 <div>
-    
+    <h3>Peraturan Terbaru</h3>
+    <br>
+    <ul>
+        <li></li>
+    </ul>
+</div>
+<div class = "kontak">
+    <h4>Kontak Kami</h4>
+    <p>Jalan Kapten A.Rivai Nomor 3
+        (0711) 352388
+        birohukumsumsel@yahoo.com
+    </p>
+    <br>
+    <p>Biro Hukum dan HAM Setda Provinsi Sumatera Selatan</p>
+
+    <br>
+    <h4>Link Terkait</h4>
+    <ul>
+        <li><a href ="instagram.com" target="_blank">Republik Indonesia</a></li>
+        <li href ="">Sekneg RI</li>
+        <li href ="">BPHN</li>
+        <li href ="">Sumatera Selatan</li>
+        <li href ="">Palembang</li>
+    </ul>
 </div>
     
 @endsection
