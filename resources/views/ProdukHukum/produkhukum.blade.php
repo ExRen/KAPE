f44a5042661e8bd135473fe4a45c8303f886b809
Valyssa Azzahra <valyssaazzahra@gmail.com>
2023-07-26 13:43:54 +0700

    fixed landing

--- resources/views/ProdukHukum/produkhukum.blade.php
+++ resources/views/ProdukHukum/produkhukum.blade.php
@@ -13,18 +13,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
 
-<<<<<<< HEAD
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="style.css" />
-        <title>Landing</title>
-    </head>
-=======
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="style.css" />
-    <link rel="icon" href="{{asset('logo/logos-01.png') }}" />
-    <title>Landing</title>
-  </head>
->>>>>>> origin/main
+
     <!-- logo atas -->
     <div class="row">
         <div class="logo">
