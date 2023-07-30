a1ca5704a92ee2e7cf57d4ed98dd0bd6f187e0d2
Bima Aryadinata <65204321+ExRen@users.noreply.github.com>
2023-07-30 14:22:08 +0700

    function

--- resources/views/admins/proses.blade.php
+++ resources/views/admins/proses.blade.php
@@ -6,7 +6,7 @@
 if (isset($_POST['aksi'])) {
     if ($_POST['aksi'] == "add") {
         echo "Tambah Data";
-    } else if ($_POST['aksi'] == "add") {
+    } else if ($_POST['aksi'] == "edit") {
         echo "Edit Data";
     }
 }
